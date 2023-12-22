aeaad9b40531f820b42d0282fa0897d7b465f7e7
Javier Andrés Villabón Morales <javillabonmo@gmail.com>
2023-12-21 20:02:54 -0500

    treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,39 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    build = ":TSUpdate",
+    event = "VeryLazy",
+    main  = "nvim-treesitter.configs",
+    opts = {
+      ensure_installed = {
+        "luadoc",
+        "lua", "vim", "vimdoc",
+        "javascript",
+        "php",
+        "html",
+        "css"
+      },
+      highlight = {
+        enable = true,
+      },
+      indent = {
+        enable = true,
+      },
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@conditional.outer",
+            ["ic"] = "@conditional.inner",
+            ["al"] = "@loop.outer",
+            ["il"] = "@loop.inner",
+          }
+        }
+      },
+    },
+  }
