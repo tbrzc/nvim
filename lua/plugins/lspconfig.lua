5571b6b82323cfc2aeb8174f45811b271b008a8d
Javier Andrés Villabón Morales <javillabonmo@gmail.com>
2023-12-25 16:53:33 -0500

    main setup

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,16 +1,21 @@
 return {
   {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "williamboman/mason.nvim",
-      'hrsh7th/cmp-nvim-lsp',
-    },
-
-  },
-  {
     'VonHeikemen/lsp-zero.nvim',
     branch = 'v3.x',
     lazy = true,
     config = false,
+  },
+  {
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      {'hrsh7th/cmp-nvim-lsp'},
+    }
+  },
+  -- Autocompletion
+  {
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      {'L3MON4D3/LuaSnip'}
+    },
   },
 }
