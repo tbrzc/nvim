5a288eb435b8098a7e9952f03cb316fd5d4b6132
Javier Andrés Villabón Morales <javillabonmo@gmail.com>
2023-12-28 13:30:29 -0500

    lazy rounded

--- lua/lazy/config.lua
+++ lua/lazy/config.lua
@@ -2,6 +2,9 @@ local config = {
     change_detection = {
         enabled = true, -- automatically check for config file changes and reload the ui
         notify = false, -- turn off notifications whenever plugin changes are made
+    },
+    ui = {
+        border = "rounded"
     }
 }
 return config
